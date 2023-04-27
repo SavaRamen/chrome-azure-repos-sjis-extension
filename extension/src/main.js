184eda91bbbde9a6866ca48c962b145a2a09890e
Yamamoto, Taizou/山本 泰三 <yamamoto.taizo@fujitsu.com>
2023-04-27 13:15:28 +0900

    refactor: bookmarklet用に//でのコメントを修正

--- extension/src/main.js
+++ extension/src/main.js
@@ -1,4 +1,4 @@
-// Azure ReposでSJISファイルを表示
+/* Azure ReposでSJISファイルを表示 */
 
 /** アプリ名 */
 const app_name = "azure repos sjis extension";
@@ -15,7 +15,8 @@ const w2s = {
   _w_decoder: new TextDecoder("windows-1252"),
   /** Windows-1252からSJISへの変換マップ */
   _w2s_map: {},
-  /** 変換マップ作成
+  /** 
+   * 変換マップ作成
    *  SJISを一文字ずつWindows-1252に変換し、Windows-1252からSJISへの変換マップを作製
    */
   _make_w2s_map(hxmin, lxmin, hxmax, lxmax) {
@@ -32,9 +33,9 @@ const w2s = {
   },
   /** 変換オブジェクト初期化 */
   init() {
-    // 半角カナ文字
+    /* 半角カナ文字領域 */
     this._make_w2s_map(0x00, 0xa1, 0x00, 0xdf);
-    // 全角文字
+    /* 全角文字領域 */
     this._make_w2s_map(0x81, 0x40, 0x9f, 0x7e);
     this._make_w2s_map(0x81, 0x80, 0x9f, 0xfc);
     this._make_w2s_map(0xe0, 0x40, 0xfc, 0x7e);
@@ -45,7 +46,7 @@ const w2s = {
     w_text = w_text || "";
     let s_text = "";
     for (let idx = 0; idx < w_text.length; idx++) {
-      // 全角文字の場合
+      /* 全角文字の場合 */
       if (idx < w_text.length - 1) {
         const w_char_2 = w_text.substr(idx, 2);
         const s_char_2 = this._w2s_map[w_char_2];
@@ -55,14 +56,14 @@ const w2s = {
           continue;
         }
       }
-      // 半角カナ文字の場合
+      /* 半角カナ文字の場合 */
       const w_char_1 = w_text.substr(idx, 1);
       const s_char_1 = this._w2s_map[w_char_1];
       if (s_char_1) {
         s_text += s_char_1;
         continue;
       }
-      // ASCII文字の場合
+      /* ASCII文字の場合 */
       s_text += w_char_1;
     }
     return s_text;
@@ -70,8 +71,10 @@ const w2s = {
   /** 要素からWindows-1252の文字列を取得 */
   get_text($element) {
     let w_text = $element.innerText;
-    // 全角文字が要素を超えて分断されている場合、次の要素が空白要素でなければ
-    // 末尾の文字と次の要素の先頭文字と組み合わせたものが変換マップにあるかを判定
+    /** 
+     * 全角文字が要素を超えて分断されている場合、次の要素が空白要素でなければ
+     * 末尾の文字と次の要素の先頭文字と組み合わせたものが変換マップにあるかを判定
+     */
     let w_next_1 = w_text[w_text.length - 1];
     let w_next_2 = !$element.nextSibling?.classList?.contains("mtkw") && $element.nextSibling?.innerText?.[0];
     if (w_next_1 && w_next_2) {
@@ -92,18 +95,21 @@ w2s.init();
 const viewer = {
   /** 書き換え処理 */
   rewrite() {
-    // files(contents, history, blame)用
-    // 全行を列挙してWindows-1252からSJISへの変換を行う、すでに変換済みは除く
+    /** files(contents, history, blame)用
+     * 全行を列挙してWindows-1252からSJISへの変換を行う、すでに変換済みは除く
+     */
     document
       .querySelectorAll(`.page-content .view-line:not([${done_attr}]`)
       .forEach(($line) => {
         $line.querySelectorAll(`span[class]`).forEach(($span) => {
           let w_text = w2s.get_text($span);
           if ($span.classList.contains("mtkw")) {
-            // 右クリックメニューの[User Preferences]-[Show and diff white space]がチェックされている場合、
-            // 空白ではなく"·"(=0xb7)になっているので、クラス名"mtkw"で判定して空白に戻す。
-            // この処理が必要なのはfilesメニューの場合だけ。
-            // commits,pushesメニューでは[User preferences]が無い。
+            /**
+             * 右クリックメニューの[User Preferences]-[Show and diff white space]がチェックされている場合、
+             * 空白ではなく"·"(=0xb7)になっているので、クラス名"mtkw"で判定して空白に戻す。
+             * この処理が必要なのはfilesメニューの場合だけ。
+             * commits,pushesメニューでは[User preferences]が無い。
+             */
             w_text = w_text.replace(/·/g, "+");
           }
           const s_text = w2s.decode_text(w_text);
@@ -115,8 +121,10 @@ const viewer = {
         });
       });
 
-    // commits, pushes用
-    // 全行を列挙してWindows-1252からSJISへの変換を行う、すでに変換済みは除く
+    /**
+     * commits, pushes用
+     * 全行を列挙してWindows-1252からSJISへの変換を行う、すでに変換済みは除く
+     */
     document
       .querySelectorAll(
         `.repos-changes-viewer .repos-line-content:not([${done_attr}])`
@@ -124,7 +132,7 @@ const viewer = {
       .forEach(($line) => {
         $line.childNodes.forEach(($node) => {
           if (!$node.tag && $node.data) {
-            // ノードがテキストの場合
+            /* ノードがテキストの場合 */
             const w_text = $node.data;
             const s_text = w2s.decode_text(w_text);
             if (w_text != s_text) {
@@ -137,7 +145,7 @@ const viewer = {
             $node.className != "screen-reader-only" &&
             !$node.ariaHidden
           ) {
-            // ノードが<SPAN>の場合
+            /* ノードが<SPAN>の場合 */
             const w_text = w2s.get_text($node);
             const s_text = w2s.decode_text(w_text);
             if (w_text != s_text) {
@@ -148,10 +156,10 @@ const viewer = {
           }
         });
       });
-    // 変換済み属性がある場合は文字単位の差分表示は間違っているため削除
+    /* 変換済み属性がある場合は文字単位の差分表示は間違っているため削除 */
     let is_done_attr = document.querySelectorAll(`[${done_attr}]`).length != 0;
     if (is_done_attr) {
-      // files(contents, history, blame)用
+      /* files(contents, history, blame)用 */
       document.querySelectorAll(".view-overlays").forEach(($element) => {
         $element
           .querySelectorAll(".char-insert, .char-delete")
@@ -159,7 +167,7 @@ const viewer = {
             $element.remove();
           });
       });
-      // commits, pushes用
+      /* commits, pushes用 */
       document.querySelectorAll(".repos-summary-diff-blocks").forEach(($element) => {
         $element
           .querySelectorAll(".added-content, .removed-content")
@@ -171,7 +179,7 @@ const viewer = {
   }
 };
 
-// ページ変更監視処理
+/* ページ変更監視処理 */
 const observer = new MutationObserver((list, observer) => {
   viewer.rewrite();
 });
